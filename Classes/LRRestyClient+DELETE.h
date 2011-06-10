3a3a8f414cb39d99edeb0ed9ca15bf165f297a7e
Barry Wilson <rufferto@mac.com>
2011-06-10 02:00:39 -0700

    Add delete to LRRestyClient API

---
+++ Classes/LRRestyClient+DELETE.h
@@ -0,0 +1,75 @@
+//
+//  LRRestyClient+DELETE.h
+//  LRResty
+//
+//  Created by Barry Wilson on 6/10/11.
+//  Copyright 2010 LJR Software Limited. All rights reserved.
+//
+
+#import "LRRestyClient.h"
+#import "LRRestyTypes.h"
+
+@class LRRestyRequest;
+
+@interface LRRestyClient (DELETE)
+
+#pragma mark -
+#pragma mark Delegate API
+
+/**
+ Performs a DELETE request on URL resource with delegate response handling.
+ @param urlString   The URL resource to delete.
+ @param delegate    The response delegate.
+ @returns The request object.
+ */
+- (LRRestyRequest *)delete:(NSString *)urlString delegate:(id<LRRestyClientResponseDelegate>)delegate;
+
+/**
+ Performs a DELETE request on URL resource with the specified request headers with delegate response handling.
+ @param urlString   The URL resource to delete.
+ @param headers     A dictionary of HTTP request headers.
+ @param delegate    The response delegate.
+ @returns The request object.
+ */
+- (LRRestyRequest *)delete:(NSString *)urlString headers:(NSDictionary *)headers delegate:(id<LRRestyClientResponseDelegate>)delegate;
+
+#pragma mark -
+#pragma mark Blocks API
+
+/**
+ Performs a DELETE request on URL resource with block response handling.
+ @param urlString   The URL resource to delete.
+ @param block       The response handler.
+ @returns The request object.
+ */
+- (LRRestyRequest *)delete:(NSString *)urlString withBlock:(LRRestyResponseBlock)block;
+
+/**
+ Performs a DELETE request on URL resource with block response handling.
+ @param urlString   The URL resource to delete.
+ @param headers     A dictionary of HTTP request headers.
+ @param block       The response handler.
+ @returns The request object.
+ */
+- (LRRestyRequest *)delete:(NSString *)urlString headers:(NSDictionary *)headers withBlock:(LRRestyResponseBlock)block;
+
+#pragma mark -
+#pragma mark Synchronous API
+
+/**
+ Performs a *synchronous* DELETE request on URL resource, blocking the calling thread.
+ @param urlString   The URL resource to delete.
+ @returns The response to the request.
+ */
+- (LRRestyResponse *)delete:(NSString *)urlString;
+
+/**
+ Performs a *synchronous* DELETE request on URL resource, blocking the calling thread.
+ @param urlString   The URL resource to delete.
+ @param headers     A dictionary of HTTP request headers.
+ @returns The response to the request.
+ */
+- (LRRestyResponse *)delete:(NSString *)urlString headers:(NSDictionary *)headers;
+
+
+@end
---
+++ Classes/LRRestyClient+DELETE.m
@@ -0,0 +1,67 @@
+//
+//  LRRestyClient+DELETE.m
+//  LRResty
+//
+//  Created by Barry Wilson on 6/10/11.
+//  Copyright 2010 LJR Software Limited. All rights reserved.
+//
+
+#import "LRRestyClient+DELETE.h"
+#import "LRRestyClientProxyDelegate.h"
+#import "LRRestyClientBlockDelegate.h"
+#import "LRRestyClientStreamingDelegate.h"
+#import "NSObject+SynchronousProxy.h"
+
+@implementation LRRestyClient (DELETE)
+
+#pragma mark -
+#pragma mark Delegate API
+
+- (LRRestyRequest *)delete:(NSString *)urlString delegate:(id<LRRestyClientResponseDelegate>)delegate;
+{
+    return [self delete:urlString headers:nil delegate:delegate];
+}
+
+- (LRRestyRequest *)delete:(NSString *)urlString headers:(NSDictionary *)headers delegate:(id<LRRestyClientResponseDelegate>)delegate;
+{
+    return [HTTPClient DELETE:[NSURL URLWithString:urlString] headers:headers delegate:[LRRestyClientProxyDelegate proxyForClient:self responseDelegate:delegate]];
+}
+
+#pragma mark -
+#pragma mark Blocks API
+
+- (LRRestyRequest *)delete:(NSString *)urlString withBlock:(LRRestyResponseBlock)block;
+{
+    return [self delete:urlString headers:nil withBlock:block];
+}
+
+- (LRRestyRequest *)delete:(NSString *)urlString headers:(NSDictionary *)headers withBlock:(LRRestyResponseBlock)block;
+{
+    return [HTTPClient DELETE:[NSURL URLWithString:urlString] headers:headers delegate:[LRRestyClientBlockDelegate delegateWithBlock:block]];
+}
+
+#pragma mark -
+#pragma mark Synchronous API
+
+- (LRRestyResponse *)delete:(NSString *)urlString;
+{
+    return [self performAsynchronousBlockAndReturnResultWhenReady:^(id *result) 
+            {
+                [self delete:urlString withBlock:^(LRRestyResponse *response) {
+                    *result = [response retain];
+                }];
+            }];
+}
+
+- (LRRestyResponse *)delete:(NSString *)urlString headers:(NSDictionary *)headers;
+{
+    return [self performAsynchronousBlockAndReturnResultWhenReady:^(id *result) 
+            {
+                [self delete:urlString headers:headers withBlock:^(LRRestyResponse *response) {
+                    *result = [response retain];
+                }];
+            }];
+}
+
+
+@end
