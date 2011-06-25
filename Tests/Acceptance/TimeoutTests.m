7dfad59ca2dddbc350a8dadd55f061c3daa0c564
Luke Redpath <luke@lukeredpath.co.uk>
2011-06-25 18:35:02 +0100

    Share the key project schemes.

---
+++ Tests/Acceptance/TimeoutTests.m
@@ -0,0 +1,23 @@
+//
+//  TimeoutTests.m
+//  LRResty
+//
+//  Created by Luke Redpath on 25/06/2011.
+//  Copyright 2011 LJR Software Limited. All rights reserved.
+//
+
+#import "TimeoutTests.h"
+
+@implementation TimeoutTests
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
