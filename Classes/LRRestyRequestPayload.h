2ad8be2fc2042cc2e07a7db13e40fe0a509ec72c
Luke Redpath <luke@lukeredpath.co.uk>
2011-06-26 02:47:03 +0100

    Improved payload system documentation

--- Classes/LRRestyRequestPayload.h
+++ Classes/LRRestyRequestPayload.h
@@ -17,15 +17,58 @@
  take a payload parameter.
  
  When you perform POST or PUT requests to a resource, you typically want to post some kind of
- data to that resource.
- 
- The LRResty payload system lets you POST or PUT data in an agnostic way, letting you pass
+ data to that resource. The LRResty payload system lets you POST or PUT data in an agnostic way, letting you pass
  any object that implements the LRRestyRequestPayload protocol into one of the request methods
  on LRRestyClient that accept a payload parameter.
  
- In addition, LRResty provides out of the box support for certain native data types, including
- encodable objects (such as NSString, or anything that responds to dataUsingEncoding:), NSDictionary
+ #### Native type support
+ 
+ LRResty provides out of the box support for certain native data types, including
+ NSString (or any other object that responds to dataUsingEncoding:), NSDictionary
  and of course, NSData can also be used directly.
+ 
+ #### Implementing the protocol in your domain model
+ 
+ As an example, imagine you have a model object that you would like to serialize as JSON and POST
+ to a server that uses a REST API. Your model object could implement the LRRestyRequestPayload directly:
+ 
+    @interface SpaceShip
+      ...
+    
+    - (NSData *)dataForRequest
+    {
+      return [[self propertiesAsDictionary] JSONDataUsingEncoding:NSUTF8Encoding];
+    }
+ 
+    - (NSString *)contentTypeForRequest
+    {
+      return @"application/json";
+    }
+ 
+    @end
+ 
+ It is now possible to POST or PUT an instance of your SpaceShip object to your server using the
+ LRRestyClient APIs without any further work:
+ 
+    SpaceShip *spaceship = [self findInstanceOfSpaceShip];
+  
+    [[LRResty client] post:@"http://www.nasa.com/spaceships" payload:spaceship withBlock:^(LRRestyResponse *response) {
+        if (response.code == 201) {
+            NSLog(@"Spaceship was created on server!");
+        }
+    }];
+ 
+ Finally, if the REST API was strict and required that the Accept header was also set correctly,
+ we can simply extend our class to implement modifyRequestBeforePerforming: as well:
+ 
+    @implementation SpaceShip (Continued)
+ 
+    - (void)modifyRequestBeforePerforming:(LRRestyRequest *)request
+    {
+      [request addHeader:@"Accept" value:@"application/json"];
+    }
+ 
+    @end
  */
 @protocol LRRestyRequestPayload <NSObject>
 
